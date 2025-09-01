3ead1b40d8fa38115926e70e9f2f4a01c76ed862
sibi_tiruchi <sibi.tiruchi@gmail.com>
2025-09-01 02:39:00 -0400

    final_fix

---
+++ src/util/axiosConfig.jsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { BASE_URL } from "./apiEndpoints";
+
+const axiosConfig = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+    }
+})
+
+// endpoints that do not require auth header
+const excludeEndpoints = ["/login", "/add", "/status", "/activate"];
+
+// request interceptor
+axiosConfig.interceptors.request.use((config) => {
+    const skipToken = excludeEndpoints.some((endpoint) => {
+        return config.url?.includes(endpoint)
+    });
+
+    if (!skipToken) {
+        const accessToken = localStorage.getItem("token");
+        if (accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+// response interceptor
+axiosConfig.interceptors.response.use((response) => {
+    return response;
+}, (error) => {
+    if (error.response) {
+        if (error.response.status === 401) {
+            window.location.href = "/login";
+        } else if (error.response.status === 500) {
+            console.error("Server error. Try again later.");
+        }
+    } else if (error.code === "ECONNABORTED") {
+        console.error("Request timeout. Try again.");
+    }
+    return Promise.reject(error);
+});
+
+export default axiosConfig;
